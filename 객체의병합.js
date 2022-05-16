b8e14718a56f5324fb1196e71569d98fb63f7772
gaeun <gaeun2937@gmail.com>
2022-05-17 00:09:01 +0900

    객체의 병합

--- 객체의병합.js
+++ 객체의병합.js
@@ -55,20 +55,25 @@ console.log(sourceObject.traits.first_name.source_id);
 
 newObject1.traits.first_name.source_id = 100;
 
-console.log(sourceObject.traits.first_name.source_id);
+console.log(sourceObject.traits.first_name.source_id); // newObject1가 깊은 복사가 되었음을 알 수 있다.
 
 newObject2.traits.first_name.source_id = 100;
 
-console.log(sourceObject.traits.first_name.source_id);
+console.log(sourceObject.traits.first_name.source_id); // newObject2는 얕은 복사가 되어 안쪽에 있는 객체는 참조가 되었음을 알 수 있다. => 확정적 버그
 
 newObject3.traits.first_name.source_id = 500;
 
-console.log(sourceObject.traits.first_name.source_id);
+console.log(sourceObject.traits.first_name.source_id); // newObject3 역시 얕은 복사임을 알 수 있다.
 
+// -깊은 복사
+// (2) 복사하는 유틸리티 함수를 직접 만들기
 function deepCopyObject(obj){
     const clone = {};
     for (const key in obj){
+        // 복사할지 말지 결정
+        // 객체가 아닐때까지 반복됨
         if(typeof obj[key] == "object" && obj[key] != null){
+            // 재귀호출
             clone[key] = deepCopyObject(obj[key]);
         } else {
             clone[key] = obj[key];
@@ -83,7 +88,14 @@ console.log(sourceObject.traits.first_name.source_id);
 
 newObject4.traits.first_name.source_id = 1000;
 
-console.log(sourceObject.traits.first_name.source_id);
+console.log(sourceObject.traits.first_name.source_id); // 원본이 바뀌지 않음을 확인할 수 있음(그러므로 깊은복사가 되었음을 확인할 수 있다.)
+
+// 병합을 하거나 새로운 객체를 만들 때 참조를 끊는 것이 가장 중요하다.
+//====================
+
+// 새로운 오브젝트를 만들기
+// store 정보 중 일부를 그대로 유지하고
+// 다른 정보를 대체할 하는 코드
 
 const store = {
     user: null,
@@ -94,9 +106,22 @@ const store = {
     }
 }
 
+
+
+
+// 새로운 객체를 만들고
 const newObject5 = {
+    // store를 깊은 복사를 한다.
+    // => 전개 연산자를 이용해 넣고
     ...deepCopyObject(store),
+
+    // 변경할 객체의 속상만 다시 재구성한다.
     config:{
+        // 기존 store의 config 안의 속성값은
+        // 전개 연산자를 통해 다시 펼쳐 넣어주어야 한다.
+        // 왜냐? 그렇지 않으면
+        // 위에서 깊은 복사를 할 때 config를 덮어쓰면서
+        // multiDevice 속성이 사라지게 때문에
         ...store.config,
         lastLoginDate: new Date(),
     },
@@ -104,6 +129,9 @@ const newObject5 = {
 
 console.log(newObject5);
 
+// 디폴드 값을 설정하는 테크닉
+//      Object copy 연산 콘셉을 이용하여
+//      디폴트 연산을 수행하는 방법을 적용하면 된다.
 const DefaultStyle = {
     color: '#fff',
     contColor: '#999',
@@ -111,7 +139,12 @@ const DefaultStyle = {
     fontWeight: 200,
 };
 
+// 어떤 함수의 입력값을 객체로 받고
+// 그 객체의 대부분의 속성들이 디폴트 값을 제공하는 형식이 됐을 때
+// 이와 같은 패턴을 이용하면 매우 편한 코드를 작성할 수 있다.
 function createParagraph(config){
+    // 원래 디폴트 값을 넣고,
+    // config로 받은 값을 넣어 overwrite 한다.
     config = {...DefaultStyle, ...config};
 
     console.log(config);
